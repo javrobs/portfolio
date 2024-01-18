d6a24187ee1f5c47f3cfc2e4715f9963357ab347
javrobs <javieroblesamar@gmail.com>
2024-01-18 13:21:05 +0800

    minimenu working

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,7 @@
 const allPages=document.querySelectorAll(".page");
+const collapsed=document.querySelectorAll(".collapsible");
+const minimenuButton=document.getElementById("minimenu-button");
+const icon=minimenuButton.querySelector("i");
 
 function activatePage(pageId){
     console.log(pageId);
@@ -10,4 +13,25 @@ function activatePage(pageId){
             one.classList.remove("active-page");
         }
     });
+}
+
+function expandMiniMenu(){ 
+    collapsed.forEach(one=>{
+        one.classList.add("expanded");
+    });
+    minimenuButton.setAttribute("onclick","collapseMiniMenu();");
+    icon.className="bi bi-chevron-compact-up";
+}
+
+function collapseMiniMenu(){
+    collapsed.forEach(one=>{
+        one.classList.remove("expanded");
+    });
+    minimenuButton.setAttribute("onclick","expandMiniMenu();");
+    icon.className="bi bi-chevron-compact-down";
+}
+
+function activatePageMini(pageId){
+    activatePage(pageId);
+    collapseMiniMenu();
 }
