6cbc97bc440b4f787582c00556a1f9a98fdc9560
Javier Robles <javieroblesamar@gmail.com>
2024-08-08 19:31:33 -0400

    project links up and running

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,8 @@ const collapsed=document.querySelectorAll(".collapsible");
 const minimenuButton=document.getElementById("minimenu-button");
 const icon=minimenuButton.querySelector("i");
 
+
+
 function activatePage(pageId){
     console.log(pageId);
     allPages.forEach(one=>{
@@ -34,4 +36,44 @@ function collapseMiniMenu(){
 function activatePageMini(pageId){
     activatePage(pageId);
     collapseMiniMenu();
+}
+
+function roxiesWebsite(){
+    document.getElementById('me-and-roxie-photo').classList.add('change-to-roxie');
+}
+
+
+let divsToDrop=[['greeting',1],['p-1',3],['p-2',3],['p-3',3]];
+startLetterDropper(...divsToDrop[0])
+
+function startLetterDropper(itemID,speed){
+    let item=document.getElementById(itemID);
+    let textToDrop=item.innerHTML;
+    item.innerHTML='';
+    item.classList.remove('d-none')
+    dropALetter(textToDrop,item,arguments[1]);
+}
+
+function dropALetter(text,item,speed){
+    if(text.length>0){
+        let letterToDrop=text.substring(0,1);
+        text=text.substring(1);
+        item.innerHTML+=letterToDrop;
+        switch(letterToDrop){
+            case ',':
+                setTimeout(dropALetter.bind(this,...arguments),200/speed);
+                break;
+            case '.':
+                setTimeout(dropALetter.bind(this,...arguments),200/speed);
+                break;
+            case ' ':
+                setTimeout(dropALetter.bind(this,...arguments),50/speed);
+                break;
+            default:
+                setTimeout(dropALetter.bind(this,...arguments),30/speed);
+        }
+    } else {
+        divsToDrop.shift()
+        if(divsToDrop.length)setTimeout(startLetterDropper.bind(this,...divsToDrop[0]),1000);
+    }
 }
